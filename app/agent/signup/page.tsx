2795f1b9936dcef25abfc1b329dec63ad1316483
templeajuzie <kliptolite@gmail.com>
2025-04-22 16:07:41 +0100

    feat: update

--- app/agent/signup/page.tsx
+++ app/agent/signup/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import AgentSignupForm from "@/components/auth/agent-signup-form";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 export default function SignupPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-[#0F0F12] p-4">
-      <AgentSignupForm />
+      <Suspense fallback={<LoadingSpinner/>}>
+        <AgentSignupForm />
+      </Suspense>
     </div>
   );
 }
--- app/buyer/signup/page.tsx
+++ app/buyer/signup/page.tsx
@@ -1,9 +1,13 @@
+import { Suspense } from "react";
 import SignupForm from "@/components/auth/buyer-signup-form";
+import { LoadingSpinner } from "@/components/ui/loading-spinner";
 
 export default function SignupPage() {
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 dark:bg-[#0F0F12] p-4">
-      <SignupForm />
+      <Suspense fallback={<LoadingSpinner />}>
+        <SignupForm />
+      </Suspense>
     </div>
   );
 }
